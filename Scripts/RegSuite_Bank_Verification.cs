6ef92f827c8ba9f632b7836995bad6098eb8a9b1
Nelda Raju <nraju@billzy.com>
2020-02-14 05:41:27 +0000

    Test commit in bank verification cs file

--- Scripts/RegSuite_Bank_Verification.cs
+++ Scripts/RegSuite_Bank_Verification.cs
@@ -17,7 +17,7 @@ namespace BillzyAutomationTestSuite.Scripts
 {
     [TestFixture]
     [Parallelizable]
-    //Test validation
+    //Test validation to githu
     class RegSuite_Bank_Verification : Tests
     {
         [Test]
